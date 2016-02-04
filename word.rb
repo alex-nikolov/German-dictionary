26ed6d7dfc4f0f60c3e424a16d4258406e97e382
alex-nikolov <alexnickolow@gmail.com>
2016-02-04 19:39:50 +0200

    Remove unnecessary methods after switching to a db

--- word.rb
+++ word.rb
@@ -1,26 +1,11 @@
 module German
   class Word
-    attr_accessor :entry, :meaning, :type
+    attr_accessor :entry, :meaning, :examples
 
-    def initialize(description)
-      @entry = extract_entry(description)
-      @meaning = extract_meaning(description)
-      @type = extract_type(description)
-    end
-
-    private
-
-    def extract_entry(description)
-      @entry = description.partition('*').first[1..-1]
-    end
-
-    def extract_meaning(description)
-      @meaning = description.partition('*')[1..2].join
-    end
-
-    def extract_type(description)
-      word_match = /.+\*(verb|noun|adj)\*.+/.match description
-      @type = word_match[1]
+    def initialize(entry, meaning, examples)
+      @entry = entry
+      @meaning = meaning
+      @examples = examples
     end
   end
 end

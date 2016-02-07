cc7103b94d1beb9fd0b8233f566a99416cfd9ef4
alex-nikolov <alexnickolow@gmail.com>
2016-02-07 19:45:02 +0200

    Add score and guess methods to Quiz class

---
+++ quiz.rb
@@ -0,0 +1,86 @@
+require 'sqlite3'
+
+require_relative 'noun'
+require_relative 'verb'
+require_relative 'adjective'
+
+module German
+  class Quiz
+    attr_accessor :words_to_be_guessed
+
+    def initialize(parts_of_speech, database, fields_to_be_guessed)
+      @database = database
+      @words_to_be_guessed = load_words_from_database(parts_of_speech)
+
+      @current_word = @words_to_be_guessed[0] if @words_to_be_guessed.size > 0
+      @fields_to_be_guessed = fields_to_be_guessed
+      @right_answers = 0
+      @total_answers = 0
+    end
+
+    def guess(suggestions)
+      suggestions_with_fields = suggestions.zip @fields_to_be_guessed
+      suggestion_correctness = suggestions_with_fields.map do |pair|
+        correct_answer = @current_word.method(pair.last).call
+        evaluate_correctness_of_suggestion(pair, correct_answer)
+      end
+      update_score(suggestion_correctness)
+      @current_word = @words_to_be_guessed.shuffle.pop
+
+      suggestion_correctness
+    end
+
+    def score
+      @total_answers == 0 ? 0 : @right_answers / @total_answers
+    end
+
+    private
+
+    def update_score(suggestion_correctness)
+      right_answers = 0
+      total_answers = suggestion_correctness.size
+
+      suggestion_correctness.each do |correctness|
+        right_answers += 1 unless correctness == 0
+      end
+
+      @right_answers += right_answers / total_answers.to_f
+      @total_answers += 1
+    end
+
+    def evaluate_correctness_of_suggestion(suggestion_with_field, correct_answer)
+      if suggestion_with_field.first == correct_answer
+        1
+      elsif /\b#{suggestion_with_field.first}\b/ =~ correct_answer
+        correct_answer
+      else
+        0
+      end
+    end
+
+    def load_words_from_database(parts_of_speech)
+      words = SQLite3::Database.open @database
+      words.results_as_hash = true
+
+      loaded_words = map_words_to_parts_of_speech(words, parts_of_speech)
+
+      words.close if words
+      loaded_words
+    end
+
+    def map_words_to_parts_of_speech(database, parts_of_speech)
+      loaded_words = []
+
+      parts_of_speech.map do |part_of_speech|
+        run_statement = database.execute "SELECT * From #{part_of_speech}s"
+        current_class = Object.const_get('German::' + part_of_speech)
+
+        run_statement.each { |row| loaded_words << current_class.new(row) }
+      end
+
+      loaded_words
+    end
+  end
+end
+
+# rspec quiz_spec.rb --require ./quiz.rb --colour --format documentation
---
+++ quiz_spec.rb
@@ -0,0 +1,115 @@
+describe 'Quiz' do
+  describe '#initialize' do
+    it 'loads entries from a single table' do
+      quiz = German::Quiz.new(['Noun'], 'quiz_examples.db', ['meaning'])
+      expect(quiz.words_to_be_guessed.size).to eq 3
+
+      entries = quiz.words_to_be_guessed.map { |word| word.entry }
+      expect(entries).to include 'Hund'
+      expect(entries).to include 'Katze'
+      expect(entries).to include 'Haut'
+    end
+
+    it 'loads entries from every table' do
+      quiz = German::Quiz.new(['Noun', 'Verb', 'Adjective'], 'quiz_examples.db', ['meaning'])
+      expect(quiz.words_to_be_guessed.size).to eq 7
+
+      entries = quiz.words_to_be_guessed.map { |word| word.entry }
+      expected_entries = ['anschauen', 'Hund', 'Katze', 'Haut', 'schön', 'kalt', 'hübsch']
+      expect((expected_entries - entries).empty?).to be true
+    end
+  end
+
+  describe '#guess' do
+    it "returns a single element array with '1' as its element when field is correctly guessed" do
+      quiz = German::Quiz.new(['Noun', 'Verb', 'Adjective'], 'quiz_examples.db', ['meaning'])
+      guess = quiz.guess(['dog'])
+      expect(guess).to eq [1]
+      #expect(quiz.score).to eq 1
+    end
+
+    it "returns a single element array with '0' as its element when field is incorrectly guessed" do
+      quiz = German::Quiz.new(['Noun', 'Verb', 'Adjective'], 'quiz_examples.db', ['meaning'])
+      guess = quiz.guess(['god'])
+      expect(guess).to eq [0]
+      #expect(quiz.score).to eq 0
+    end
+
+    it "returns a single element array with the correct answer as its element when field is partially guessed" do
+      quiz = German::Quiz.new(['Adjective', 'Noun', 'Verb'], 'quiz_examples.db', ['meaning'])
+      guess = quiz.guess(['lovely'])
+      expect(guess).to eq ['beautiful, handsome, lovely, great']
+      #expect(quiz.score).to eq 1
+    end
+
+    it "returns a two element array with two 1-s when both fields are correctly guessed" do
+      quiz = German::Quiz.new(['Noun'], 'quiz_examples.db', ['gender', 'plural'])
+      guess = quiz.guess(['der', 'Hunde'])
+      expect(guess).to eq [1, 1]
+      #expect(quiz.score).to eq 1
+    end
+
+    it "returns a two element array with two 0-s when both fields are incorrectly guessed" do
+      quiz = German::Quiz.new(['Noun'], 'quiz_examples.db', ['gender', 'plural'])
+      guess = quiz.guess(['die', 'Hunden'])
+      expect(guess).to eq [0, 0]
+      #expect(quiz.score).to eq 0
+    end
+
+    it "returns the correct two element array with when one field is correctly guessed and the other is incorrectly guessed" do
+      quiz = German::Quiz.new(['Noun'], 'quiz_examples.db', ['gender', 'plural'])
+      guess = quiz.guess(['der', 'Hunden'])
+      expect(guess).to eq [1, 0]
+      #expect(quiz.score).to eq 0
+    end
+
+    it "returns the correct two element array with when one field is incorrectly guessed and the other is partially guessed" do
+      quiz = German::Quiz.new(['Adjective'], 'quiz_examples.db', ['comparative', 'meaning'])
+      guess = quiz.guess(['schöner', 'beautiful'])
+      expect(guess).to eq [1, 'beautiful, handsome, lovely, great']
+      #expect(quiz.score).to eq 0
+    end
+  end
+
+  describe '#score' do
+    it "returns a score of 0 when no guesses are made" do
+      quiz = German::Quiz.new(['Noun', 'Verb', 'Adjective'], 'quiz_examples.db', ['meaning'])
+      expect(quiz.score).to eq 0
+    end
+    it "returns a score of 1 for correct guesses" do
+      quiz = German::Quiz.new(['Noun', 'Verb', 'Adjective'], 'quiz_examples.db', ['meaning'])
+      guess = quiz.guess(['dog'])
+      expect(quiz.score).to eq 1
+    end
+
+    it "returns a score of 0 for incorrect guesses" do
+      quiz = German::Quiz.new(['Noun', 'Verb', 'Adjective'], 'quiz_examples.db', ['meaning'])
+      guess = quiz.guess(['god'])
+      expect(quiz.score).to eq 0
+    end
+
+    it "returns a score of 1 for partial guesses" do
+      quiz = German::Quiz.new(['Adjective', 'Noun', 'Verb'], 'quiz_examples.db', ['meaning'])
+      guess = quiz.guess(['lovely'])
+      expect(quiz.score).to eq 1
+    end
+
+    it "returns a score of 1 when both fields are correclty guessed" do
+      quiz = German::Quiz.new(['Noun'], 'quiz_examples.db', ['gender', 'plural'])
+      guess = quiz.guess(['der', 'Hunde'])
+      expect(quiz.score).to eq 1
+    end
+
+    it "returns a score of 0 when neither field is correctly guessed" do
+      quiz = German::Quiz.new(['Noun'], 'quiz_examples.db', ['gender', 'plural'])
+      guess = quiz.guess(['die', 'Hunden'])
+      expect(quiz.score).to eq 0
+    end
+
+    it "returns a score of 0.5 when one of two fields is correctly guessed" do
+      quiz = German::Quiz.new(['Noun'], 'quiz_examples.db', ['gender', 'plural'])
+      guess = quiz.guess(['der', 'Hunden'])
+      expect(quiz.score).to eq 0.5
+    end
+  end
+end

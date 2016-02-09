d5a1576991be13b5d7e49aa66df3fac45925a21b
alex-nikolov <alexnickolow@gmail.com>
2016-02-10 00:40:06 +0200

    Add gui for adding words to the dictionary

--- main_gui.rb
+++ main_gui.rb
@@ -13,9 +13,33 @@ module German
 
     def draw_buttons
       @shoes.flow(margin: 20, width: 1.0, height: 0.15, displace_top: 0.03) do
-        draw_view_word_button(self, dictionary)
-        draw_add_word_button
+        draw_view_word_button(self)
+        draw_add_word_button(self)
         draw_edit_word_button
+      end
+    end
+
+    def draw_view_word_button(caller)
+      @shoes.button 'View word', width: 0.15, height: 1.0 do
+        @shoes.window(title: 'View word', width: 400, height: 300) do
+          new_window = German::GUI.new(self, caller.dictionary_database, 
+                                             caller.highscore_database)
+          new_window.draw_view_word_button_details
+        end
+      end
+    end
+
+    def draw_view_word_button_details
+      @shoes.stack do
+        @shoes.para 'Enter word', align: 'center'
+
+        @shoes.flow do
+          @edit_line = @shoes.edit_line(width: 140, align: 'right')
+
+          @go = @shoes.button 'Go', align: 'right'
+
+          display_extracted_word
+        end
       end
     end
 
@@ -35,33 +59,92 @@ module German
       end
     end
 
-    def draw_view_word_button_details
-      @shoes.stack do
-        @shoes.para 'Enter word', align: 'center'
-
-        @shoes.flow do
-          @edit_line = @shoes.edit_line(width: 140, align: 'right')
-
-          @go = @shoes.button 'Go', align: 'right'
-
-          display_extracted_word
-        end
-      end
-    end
-
-    def draw_view_word_button(caller, dictionary)
-      @shoes.button 'View word', width: 0.15, height: 1.0 do
-        @shoes.window(title: 'View word', width: 400, height: 300) do
+    def draw_add_word_button(caller)
+      @shoes.button 'Add word', width: 0.15, height: 1.0 do
+        @shoes.window(title: 'Add word', width: 400, height: 300) do
           new_window = German::GUI.new(self, caller.dictionary_database, 
                                              caller.highscore_database)
-          new_window.draw_view_word_button_details
+          new_window.draw_add_word_button_details
         end
       end
     end
 
-    def draw_add_word_button
-      @shoes.button 'Add word', width: 0.15, height: 1.0 do
+    def draw_add_word_button_details
+      @shoes.stack do
+        @shoes.flow do
+          @shoes.para 'Enter new word '
+
+          @edit_line = @shoes.edit_line(width: 140)
+        end
+
+        @shoes.flow do
+          @shoes.para 'Choose part of speech '
+
+          box = @shoes.list_box items: ['Noun', 'Verb', 'Adjective'], width: 100,
+                                choose: 'Noun'
+
+          @go = @shoes.button 'Go', align: 'right' do
+            handle_different_parts_of_speech(@edit_line.text, box.text)
+            #@shoes.close if box.text == 'Noun'
+          end
+        end
       end
+    end
+
+    def handle_different_parts_of_speech(word, part_of_speech)
+      if @dictionary.exists_entry? word
+        puts "Word '#{word}' already exists"
+        return  
+      end
+
+      case part_of_speech
+        when 'Noun' then add_noun(word)
+        when 'Verb' then add_verb(word)
+        when 'Adjective' then add_adjective(word)
+      end
+    end
+
+    def add_noun(word)
+      field_names = ['gender', 'plural', 'genetive', 'meaning', 'examples']
+      @shoes.stack do
+        edit_lines = field_names.map { |f| draw_edit_lines_for_field(f) }
+
+        draw_add_word_button_and_execute(word, edit_lines, field_names)
+      end
+    end
+
+    def draw_add_word_button_and_execute(word, edit_lines, field_names)
+      @shoes.button 'Add word' do
+        @field_data = collect_line_data(edit_lines)
+
+        @field_data.unshift(word)
+
+        field_names.map! { |field| field.capitalize }.unshift('Entry')
+        field_names = field_names.join(',').gsub('Case', 'Used_case').split(',')
+
+        word_hash = Hash[field_names.zip @field_data]
+
+        new_noun = Noun.new(word_hash)
+        add_new_word_and_print_success_message(new_noun)
+      end
+    end
+
+    def collect_line_data(edit_lines)
+      edit_lines.map { |edit_line| edit_line.text }
+    end
+
+    def draw_edit_lines_for_field(field_name)
+      @shoes.flow do
+        @shoes.para "#{field_name.capitalize} "
+        @edit_line = @shoes.edit_line(width: 100)
+      end
+
+      @edit_line
+    end
+
+    def add_new_word_and_print_success_message(new_word)
+      @dictionary.add_entry(new_word)
+      @shoes.para "Word successfully added"
     end
 
     def draw_edit_word_button
